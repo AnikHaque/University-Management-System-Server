5860bef0817e67db422cfbd0dd4921f005617887
AnikHaque <ekramul15-811@diu.edu.bd>
2024-11-27 22:18:58 +0600

    created zod validation

---
+++ src/app/modules/student/student.validation.ts
@@ -0,0 +1,53 @@
+import { z } from 'zod';
+
+const userNameValidationSchema = z.object({
+  firstName: z.string().nonempty('First name is required'),
+  middleName: z.string().optional(),
+  lastName: z.string().nonempty('Last name is required'),
+});
+
+const guardianValidationSchema = z.object({
+  fatherName: z.string().nonempty("Father's name is required"),
+  fatherOccupation: z.string().nonempty("Father's occupation is required"),
+  fatherContactNo: z.string().nonempty("Father's contact number is required"),
+  motherName: z.string().nonempty("Mother's name is required"),
+  motherOccupation: z.string().nonempty("Mother's occupation is required"),
+  motherContactNo: z.string().nonempty("Mother's contact number is required"),
+});
+
+const localGuardianValidationSchema = z.object({
+  name: z.string().nonempty("Local guardian's name is required"),
+  occupation: z.string().nonempty("Local guardian's occupation is required"),
+  contactNo: z.string().nonempty("Local guardian's contact number is required"),
+  address: z.string().nonempty("Local guardian's address is required"),
+});
+
+const studentValidationSchema = z.object({
+  id: z.string().optional(),
+  name: userNameValidationSchema,
+  gender: z.enum(['male', 'female', 'other'], {
+    required_error: 'Gender is required',
+  }),
+  dateOfBirth: z.string().optional(),
+  email: z
+    .string()
+    .email('Invalid email address')
+    .nonempty('Email is required'),
+  contactNo: z.string().nonempty('Contact number is required'),
+  emergencyContactNo: z
+    .string()
+    .nonempty('Emergency contact number is required'),
+  bloogGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'], {
+    required_error: 'Blood group is required',
+  }),
+  presentAddress: z.string().nonempty('Present address is required'),
+  permanentAddres: z.string().nonempty('Permanent address is required'),
+  guardian: guardianValidationSchema,
+  localGuardian: localGuardianValidationSchema,
+  profileImg: z.string().optional(),
+  isActive: z.enum(['active', 'blocked'], {
+    required_error: 'Status is required',
+  }),
+});
+
+export default studentValidationSchema;
